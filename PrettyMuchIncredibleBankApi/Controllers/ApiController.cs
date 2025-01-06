bb919656210d4fa932e8a588d00142d9f806c4eb
Patryk Mirosław <pmi@mindsandco.com>
2025-01-06 11:21:33 +0100

    PMI - Inital barebones commit

---
+++ PrettyMuchIncredibleBankApi/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PMI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ApiController : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [Produces(typeof(string))]
+    public Task<ActionResult> Get()
+    {
+        return Task.FromResult<ActionResult>(Ok());
+    }
+    
+}
---
+++ PrettyMuchIncredibleBankApi/Program.cs
@@ -0,0 +1,19 @@
+﻿using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+
+var app = builder.Build();
+app.MapControllers();
+app.MapOpenApi();
+app.MapScalarApiReference(cfg =>
+{
+    cfg.WithTheme(ScalarTheme.Moon)
+        .WithTitle("Pretty Much Incredible Bank Api")
+        .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+});
+
+app.UseHttpsRedirection();
+await app.RunAsync();
